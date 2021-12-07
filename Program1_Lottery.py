c7859f14ae0a7d717a982eb0e5c91729b68e23f6
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-12-08 00:06:05 +0800

    -moved the code to def function + some text.

--- Program1_Lottery.py
+++ Program1_Lottery.py
@@ -1,31 +1,32 @@
 import random
+
 while True:
-#Create a program that ask 3 numbers (0-9) from the user.
- print("Give 3 numbers ranging from 0-9")
- Number_1=int(input("Enter your first number: "))
- Number_2=int(input("Enter your second number: "))
- Number_3=int(input("Enter your third number: "))
- your_number = Number_1, Number_2, Number_3
-
-#Generate 3 random winning numbers (0-9)
- winning_num1= random.randint(0,9)
- winning_num2= random.randint(0,9)
- winning_num3= random.randint(0,9)
- winning_numbers= winning_num1, winning_num2, winning_num3
- print(f"winning numbers: {winning_numbers}")
-
-#Display “Winner” if all 3 input numbers matched the generated numbers
- if winning_numbers == your_number:
-    print("win")
- else:
-    print("lost")    
-#Display ”You loss” if not
-#Display ”Try again y/n” after each game
- choice=input("do you want to try again? y/n ")
+ def User_Input():
+  print("Give 3 numbers ranging from 0-9.")
+  Number_1=int(input("Enter your first number: "))
+  Number_2=int(input("Enter your second number: "))
+  Number_3=int(input("Enter your third number: "))
+  return Number_1, Number_2, Number_3
+ 
+ def lottery_system(N_1, N_2, N_3):
+  user_number = N_1, N_2, N_3
+  winning_num1= random.randint(0,9)
+  winning_num2= random.randint(0,9)
+  winning_num3= random.randint(0,9)
+  winning_numbers= winning_num1, winning_num2, winning_num3
+  print(f"winning numbers: {winning_numbers}")
+  if winning_numbers == user_number:
+    print("Winner")
+  else:
+    print("You lost")    
+  
+ Num1, Num2, Num3=User_Input()
+ lottery_system(Num1, Num2, Num3)
+ 
+ choice = input("do you want to try again? y/n ")
  if choice == "y":
   continue
  elif choice == "n":
   break
-#If the user enter “y” the user will play again
-#if “n” the program will exit.
+
 
