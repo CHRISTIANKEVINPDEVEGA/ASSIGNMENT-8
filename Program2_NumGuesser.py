b224d02d70fecd24c4f05f40e9ac23b9fc4cd746
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-12-08 00:21:29 +0800

    -cleaned the code

--- Program2_NumGuesser.py
+++ Program2_NumGuesser.py
@@ -1,24 +1,14 @@
-from operator import ge
 import random
 
-#Generate 1 random number (0-100)
 generated_number= random.randint(0,100)
-print(generated_number)
-#Ask the user to guess the number
-while True:
- user_answer=int(input("can you guess the number? "))
-#Display “Greater than” if the inputed number is greater than the random number
-
-
- if user_answer == generated_number:
-    print("correct")
- else:
-     if user_answer <= generated_number:
-                print("your answer is less than the generated number")
-     elif user_answer >= generated_number:
-                print("your answer is greater than the generated number")
-     if user_answer == generated_number:
-                print("correct")
-                break
-#Display “Less than” if the inputed number is less than the random number
-#Repeat asking the user until the random number has been guessed correctly
+#print(generated_number)
+user_answer=int(input("Guess a number from 0-100 that the program will generate.\n"))
+while generated_number != user_answer:
+   if user_answer < generated_number:
+      print("your answer is less than the generated number")
+      user_answer=int(input("Please guess again.\n"))
+   elif user_answer > generated_number:
+      print("your answer is greater than the generated number")
+      user_answer=int(input("Please guess again.\n"))
+if generated_number == user_answer:
+   print("That is correct, WOW amazing!")
