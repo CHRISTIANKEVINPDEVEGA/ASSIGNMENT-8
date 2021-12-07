f97308b4a70c921630c291a4e363f69c94f07ea2
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-12-07 21:30:54 +0800

    -change the range of the 3 generated numbers\n -added some text

--- Program1_Lottery.py
+++ Program1_Lottery.py
@@ -1,15 +1,16 @@
 import random
 while True:
 #Create a program that ask 3 numbers (0-9) from the user.
+ print("Give 3 numbers ranging from 0-9")
  Number_1=int(input("Enter your first number: "))
  Number_2=int(input("Enter your second number: "))
  Number_3=int(input("Enter your third number: "))
  your_number = Number_1, Number_2, Number_3
 
 #Generate 3 random winning numbers (0-9)
- winning_num1= random.randint(0,99)
- winning_num2= random.randint(0,99)
- winning_num3= random.randint(0,99)
+ winning_num1= random.randint(0,9)
+ winning_num2= random.randint(0,9)
+ winning_num3= random.randint(0,9)
  winning_numbers= winning_num1, winning_num2, winning_num3
  print(f"winning numbers: {winning_numbers}")
 
