d7c31bab460a2c9d14b2aa5f74b364640f0c201b
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-12-08 00:31:22 +0800

    -used def function in code

--- Program2_NumGuesser.py
+++ Program2_NumGuesser.py
@@ -1,14 +1,21 @@
 import random
 
-generated_number= random.randint(0,100)
-#print(generated_number)
-user_answer=int(input("Guess a number from 0-100 that the program will generate.\n"))
-while generated_number != user_answer:
+def generator():
+ generated_NUMBER= random.randint(0,100)
+ #print(generated_number)
+ return generated_NUMBER
+
+def guess_checker(generated_number):
+ user_answer=int(input("Guess a number from 0-100 that the program will generate.\n"))
+ while generated_number != user_answer:
    if user_answer < generated_number:
       print("your answer is less than the generated number")
       user_answer=int(input("Please guess again.\n"))
    elif user_answer > generated_number:
       print("your answer is greater than the generated number")
       user_answer=int(input("Please guess again.\n"))
-if generated_number == user_answer:
-   print("That is correct, WOW amazing!")
+ if generated_number == user_answer:
+   print("That is correct, WOW amazing! I'm impressed.")
+
+generated_num=generator()
+guess_checker(generated_num)   
